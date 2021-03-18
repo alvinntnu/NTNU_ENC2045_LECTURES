9dbfceba47c6910ee189c81ff3b5cfa271b19193
alvinntnu <alvinworks@gmail.com>
2021-03-18 09:02:01 +0800

    all

--- _build/jupyter_execute/nlp/ml-algorithm.py
+++ _build/jupyter_execute/nlp/ml-algorithm.py
@@ -1,14 +1,18 @@
 # Classification Models
 
 - Before the era of deep learning, probability-based classifiers are commonly used in many ML NLP tasks.
-- There are two types of probability-based models:
-    - **Generative**:
-        - Training is based on the **joint** probability of the data and the class, i.e., $P(c,d)$
-        - Examples: n-gram models, Naive Bayes, HMM, probablistic context-free grammars.
-    
-    - **Distriminative**: 
-        - Training is based on the **conditional** Probablity of the class given the data, i.e., $P(c|d)$.
-        - Examples: Logistic regression, maximum entropy models, CRF, SVM, perceptron.
+- There are two types of probability-based models: **generative** and **disriminative** models.
+
+- **Generative**:
+    - The goal of training is NOT to find $P(c|d)$. 
+    - Rather, based on the Bayes Theorem, we can estimate the joint probability of $P(c,d)$, which can reformulated as $p(d)\times p(c|d) = p(c) \times p(d|c)$
+    - That is, the training in generative models is based on the **joint** probability of the data and the class, i.e., $P(c,d)$
+    - Examples: n-gram models, Naive Bayes, HMM, probablistic context-free grammars.
+
+- **Distriminative**: 
+    - The goal of training is to directly find $P(c|d)$.
+    - Training is based on the **conditional** Probablity of the class given the data, i.e., $P(c|d)$.
+    - Examples: Logistic regression, maximum entropy models, CRF, SVM, perceptron.
 
     
 - Strengths of discriminative models:
@@ -34,6 +38,10 @@ $$
 P(Y|X) = P(Y) \frac{P(X|Y)}{P(X)} 
 $$ (naive_bayes1)
 
+### Intuition of Naive Bayes
+
+![](../images/ml-naive-bayes.gif)
+
 - In naive bayes, given a document $d$ and a class $c$, the goal is to find the **maximum joint probability** $P(c,d)$. And according bayes rule, the goal (of finding the maximum joint probability) can be reformulated as finding the maximum of the **posterior probability** of the class, $P(c|d)$:
 
 $$
@@ -50,15 +58,14 @@ C_{MAP}= \underset{c \in C}{\arg\max}\frac{P(d|c)\times P(c)}{P(d)} \\
 $$ (naive_bayes3)
 
 
+- In naive Bayes, the probabilities $P(C=c_i)$ and $P(X_i|C=c_i)$ are **parameters**. 
+- The standard, maximum likelihood, approach is to calculate these parameters (probabilities) using MLE estimators based on the training data. 
 
-- In naive Bayes, the probabilities $P(C=c_i)$ and $P(X_i|C=c_i)$ are parameters. 
-- The standard, maximum likelihood, approach is to calculate the probabilities using MLE estimators. 
-- For example, for $P(C=c_i)$, we count the cases where $C=c_i$ and divide by the sample size, same for conditional probabilities $P(X_i|C=c_i)$. 
-- To get the Bayesian maximum posterior estimate $P(C=c_i|X_i)$, we would need to assume prior distributions for the parameters, i.e., $P(X_i|C=C_i)$.
-
+- For example, for class prior probabilities, $P(C=c_i)$, we count the cases where $C=c_i$ and divide by the sample size.
+- Similarly, each feature $x_i$ of the document is in the form of a probability value of an observed event given the class, $P(x_i|C=c_i)$. For example, the probability of observing the word '好看' (**$x_i$**) when the document class is **$c_i$**. same for conditional probabilities $P(好看|C=c_i)$. 
 
 - A document needs to be vectorized through feature engineering into a numeric representation, i.e., a set of **n** features from the document, $\{x_1, x_2, ..., x_n\}$
-- Given a class $c$ and a set of **n** features from the document, $\{x_1, x_2, ..., x_n\}$, we can denote the posterior probability of each class $c$ as follows:
+- Given a class $c$ and a set of **$n$** features from the document, $\{x_1, x_2, ..., x_n\}$, we can denote the posterior probability of each class $c$ as follows:
 
 $$
 C_{MAP}= \underset{c \in C}{\arg\max}P(c|d)\\\\
@@ -81,25 +88,22 @@ C_{MAP} = \underset{c \in C}{\arg\max} P(x_1,x_2,...,x_n|c) \times P(c) \\
 = \underset{c \in C}{\arg\max} P(c) \prod_{x\in X}P(x|c)
 $$ (naive_bayes6)
 
-- Multinomial Naive Bayes is an extension of the Naive Bayes for predicting and classifying data points, where the number of distinct labels or outcomes are more than two.
+- **Multinomial Naive Bayes** is an extension of the Naive Bayes for predicting and classifying data points, where the number of distinct labels or outcomes are more than two.
 
-- In Naive Bayes, each feature $x_i$ of the document is in the form of a probability value of an observed event given the class, $P(x_i|C=c_i)$
-    - For example, the probability of observing the word '好看' (**$x_i$**) when the document class is **$c_i$**.
-
-- Issues of Smoothing
-    - For unseen features in a new document, e.g., $P(x|c)$, this would render the entire posterior probability of the class to be zero. 
+- Issues of **Smoothing**
+    - For unseen features in a new document, e.g., $P(x|c)$, this would render the entire **posterior probability** of the class to be zero. 
     - Laplace smoothing (Add-one)
     - Lidstone smooting (Self-defined $\alpha$ < 1)
 
 ## Logistic Regression
 
-- Logistic Regression is also a form of probabilistic statistical classification model.
+- **Logistic Regression** is also a form of probabilistic statistical classification model.
 - The model is trained by maximizing directly the probability of the class given the observed data, i.e., $P(c|d)$.
 - Logistic Regression is similar to linear regression, whose predicted values are both numeric.
 
-- A document needs to be vectorized through feature engineering into a numeric representation, i.e., a set of **n** features from the document, $\{x_1, x_2, ..., x_n\}$
+- A document needs to be vectorized through feature engineering into a numeric representation, i.e., a set of **$n$** features characterizing the semantics of the document, $\{x_1, x_2, ..., x_n\}$
 
-- Then the logistic regression models the probability of the class given these observed values:
+- Then the Logistic Regression models the **probability** of the class given these observed values:
 
 $$
 y = \beta_0 + \beta_1x_1 + \beta_2 x_2 + ... + \beta_ix_i
@@ -108,17 +112,19 @@ $$
 
 - Each feature $x_i$ of the document is a function that chracterizes the relevant linguistic properties of the document. These features can be all manually annotated or created automatically.
 - These features are often in simple forms that are of binary values or numeric values within a range:
+
     $$ 
-    x_1 = f_1(x_1) = [Contains('好看') ] = \{0, 1\} \\
-    x_2 = f_2(x_2) = [Contains('絕配') ] = \{0, 1\} \\
+    x_1 = f_1(x_1) = [Contains("好看") ] = \{0, 1\} \\
+    x_2 = f_2(x_2) = [Contains("絕配") ] = \{0, 1\} \\
     ...
     $$
 
 
-- But so far the $y$ is not a probability value yet. It could be any number ($-\infty$ to $\infty$) based on the current linear model. In order to make sure the predicted values of the model are within the range of 0 and 1, the dependent variable $y$ often needs to be transformed with a so-called **link function**.
-- That is, link functions transform the predicted $y$ into a range more appropriate for linear modeling.
+- But so far the model prediction ($y$) could be any number ($-\infty$ to $\infty$) based on the current linear model. 
+- In order to make sure the predicted values of the model are within the range of 0 and 1 (i.e., a probability-like value), the dependent variable $y$ often needs to be transformed with a so-called **link function**.
+- That is, **link functions** transform the predicted $y$ into a range more appropriate for linear modeling.
 
-- For binary logistic regression, the **inverse logit transformation** is used, transforming y from the range of $-\infty$ to $\infty$ to into probability values ranging from 0 to 1.
+- For binary logistic regression, the **inverse logit transformation** is used, transforming $y$ from the range of $-\infty$ to $\infty$ to into probability values ranging from 0 to 1.
 
 $$
 y = \beta_0 + \beta_1x_1 + \beta_2 x_2 + ... + \beta_ix_i
@@ -127,26 +133,65 @@ $$
 
 $$
 Probability_{class} = P(c|d) = P(c|x_1,x_2,...,x_i)\\
-= \frac{1}{1+e^y}\\
-= \frac{1}{1+e^{\sum_{i}\beta_ix_i}}
+= \frac{1}{1+e^{-y}}\\
+= \frac{1}{1+e^{-(\sum_{i}\beta_ix_i)}}
 $$
 
 - And we can re-formulate the model equation as:
 
 $$
-P(c|d) = P(c|x_1,x_2,...,x_i)\\= \frac{1}{1+e^{\sum_{i}\beta_ix_i}}
+P(c|d) = P(c|x_1,x_2,...,x_i)\\= \frac{1}{1+e^{-(\sum_{i}{\beta_ix_i})}}
 $$
-- The model will then determine the class of the document by choosing the class that maximizes the conditional probability of $P(c|d)$.
+- In model prediction, the model will then determine the **class** of the document by choosing the class that maximizes the conditional probability of $P(c|d)$.
 
    
 
+- In training:
+    - Given a document in a training set, $d$, the initial Logistic Regression model will output the predicted conditional probability of $d$ being the class, $c$, i.e., $P(c|d)$.
+    - This is commonly referred to as the **likelihood**. 
+    - And the optimal Logistic Regression is the one that maximizes the likelihoods of the entire training set.
+    - This maximum likelihood approach is similar to the least squares method in linear regression.
+
+- Interpreting the parameters (coefficients $\beta_i$)
+
+    - The coefficient refers to the change of the log odds of being the relevant class when there is one unit increase in the corresponding $x_i$ feature. The log odds change will be $e^{\beta_i}$.
+    - Log odds is defined as: 
+    
+    $$
+    log\left({\frac{P_{relevant_class}}{1 - P_{relevant_class}}}\right)
+    $$
+
+
+%load_ext rpy2.ipython
+
+import pandas as pd
+
+csv= pd.read_table('../../../RepositoryData/data/gries_sflwr/_inputfiles/05-3_clauseorders.csv')
+csv
+
+%%R -i csv
+
+# library(readr)
+# csv= read_delim('../../../RepositoryData/data/gries_sflwr/_inputfiles/05-3_clauseorders.csv','\t')
+print(head(csv))
+print(table(csv$SUBORDTYPE, csv$ORDER))
+
+%%R
+lg = glm(factor(csv$ORDER)~factor(csv$SUBORDTYPE), family="binomial")
+summary(lg)
+
+- We use the `order` of the main and subordinate clauses to predict the type of the subordinate clauses.
+- The log odds of being mc-sc order increases $e^{2.7234}= 15.23$ when the subordinate clause is `temp`.
+
 ## Support Vector Machine (SVM)
 
-![](../images/svm/svm.001.png)
 - To understand what SVM is, we probably need to introduce the idea of **support vectors**.
 - Given a linearly separable dataset, it is easy to find a hyperplane (in 2D, that would be a line) that separates data into two distinct sub-groups.
     - The shortest distance between the observations and the threshold (hyperplane) is called the **margin**.
     - A hyperplane that gives us the largest margin to make classification is referred to as **Maximum Margin Classifier**.
+    
+
+![](../images/svm/svm.001.png)
     
 
 - Issues with Maximum Margin Classifier:
@@ -160,6 +205,7 @@ $$
     - When we allow misclassifications, the distance between the observations and the hyperplane is called a **soft margin**.
     - A classifier allowing misclassifications (i.e., based on soft margins) is referred to as **soft margin classifier**, or **support vector classifier**.
     - The observations on the edge and within the **soft margin** are called **support vectors**.
+
 ![](../images/svm/svm.003.png)
 
 - Support Vector Classifiers can deal with observations with outliers and the assumption is that the observations are linearly separable.
@@ -179,27 +225,39 @@ $$
     - Radial Basis Function Kernel (RBF)
 
 
-- SVM has a **cost function (C)**, which controls the compromise of the misclassifications and margin sizes. A smaller C creates a softer (larger) margin, allowing more misclassifications; a larger C creates a narrower margin, allowing fewer misclassifications. A model of larger C often has higher generalizability.
+- SVM has a **cost function (C)**, which controls the compromise of the **misclassifications** and **margin** sizes. 
+- A smaller C creates a softer (larger) margin, allowing more misclassifications. 
+- A larger C creates a narrower margin, allowing fewer misclassifications. 
+- A model of larger C often has higher generalizability.
 
 
 
 ## Other Discriminative Models
 
-There are many more ways to learn the weights (i.e., $\beta_i$) in the discriminative models. Different algorithms may prioritize different link functions to train the model parameters.
+There are many more ways to learn the weights (i.e., $\beta_i$) in the discriminative models. Different algorithms may prioritize different **link functions** to train the model parameters.
 
 In this course, we will not cover all the details of these variants. Please refer to machine learning courses for more details on these algorithms.
 
-- **Maximum Entropy Model (Maxent)**
-    - Maxent is very similar to logistic regression, and differs in mainly its way of parameterization (e.g., feature functions, link functions)
-    - Maxent uses a *softmax* function to transform values into probabilities.
-    
+### Maximum Entropy Model (Maxent)
+
+- Maxent is very similar to logistic regression, and differs in mainly its way of parameterization (e.g., feature functions, link functions)
+- In Maxent, each feature function is a function of both the text feature and the class, i.e., $f(x_i, c_i)$.
+
 $$
-p(c|d,\beta) = \frac{e^{\sum_{i}\beta_ix_i}}{\sum_{c'}{e^{\sum_{i}\beta_ix_i}}}\\
-= \frac{y_c}{\sum_{c'}{y_{c'}}}
+f_1(c_i, x_1) \equiv [C = c_i \land Contains("好看")]\\
+f_2(c_i, x_2) \equiv [C = c_i \land Contains("絕配")]\\
+f_3(c_i, x_3) \equiv [C = c_1 \land Ends(w,"c")]\\
+...
 $$
 
-- **Perceptron**
-    - finds a currently misclassified example, and nudge weights in the direction of its correct classification.
+- The model will assign to each feature a **weight**.
+- Maxent uses a *softmax* function to transform the sum of the weigthed feature values into probabilities. (The exponetial transformation of the sum of the weighted feature values make all predicted values positive.)
+   - Numerator = The probability of the feature i co-occuring with specific class.
+   - Denominator = The sum of the probability of the feature co-occurring with the other classes.
+   
+$$
+p(c|d,\lambda) = \frac{e^{\sum_{i}\lambda_if_i(c,x_i)}}{\sum_{c'}{e^{\sum_{i}\lambda_if_i(c',x_i)}}}\\
+$$
 
 ## Other Classification Models
 
@@ -235,3 +293,4 @@ $$
 
 - NLTK Book, Chapter 6.
 - Sarkar (2020), Chapter 5.
+- Geron (2019), Chapter 5-7
