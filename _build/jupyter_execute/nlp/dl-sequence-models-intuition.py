271d44d750a89be8a37444057ab9808ad42c3dd1
alvinntnu <alvinworks@gmail.com>
2021-05-15 16:15:30 +0800

    all

--- _build/jupyter_execute/nlp/dl-sequence-models-intuition.py
+++ _build/jupyter_execute/nlp/dl-sequence-models-intuition.py
@@ -8,36 +8,37 @@
 
 # ## Why Sequence Models?
 # 
-# - Humans process texts in a sequential order (from left to right)
+# - Humans process texts in a sequential order (from left to right).
 # - When we process texts and make classification, we utilize our reasoning about previous sequences to inform later decision making.
 
 # - **Recurrent neural network** (RNN) addresses this issue by implementing networks with loops that allow information to persist.
-# - The loop allows information to be passed from previous time step to the next time step.
+# - The loop allows information to be passed from the previous time step to the next time step.
 # 
 # ![](../images/s2s-rnn.jpeg)
 
 # ## Neural Network
 # 
-# - Neural network expects an numeric input, i.e., a numeric representation of the input text.
+# - Neural network expects an numeric input, i.e., a numeric representation of the input text/word.
 # - So the first step in deep learning is the same as traditional ML, which is text vectorization.
-# - And because a sequence model eats in one word as the input, word representation/vectorization is crucial.
+# - And because a sequence model like RNN eats in one **word** at a time, **word vectorization** is necessary and crucial.
 # 
 
-# ## Word Representations in Deep Learning
+# ## Word Representations in Sequence Models
 
 # - Like in traditional machine learning, feature engineering is crucial to the success of the computational modeling.
-# - Of particular importance is the transformation of each text into numeric representation that has a significant portion of **textual semantics**.
+# - Of particular importance is the transformation of each text/word into numeric representation that has a significant portion of its **semantics**.
 
-# - One-hot encoding is the most intuitive way to represent lexical words numerically.
-# - If the language vocabulary size is *V*, each word can be represented as a vector of size *V*, with its correpsonding dimension to be the value of **1** and the rest being **0**'s.
+# - **One-hot encoding** is the simplest way to represent texts/words numerically.
+# - If the corpus vocabulary size is *V*, each word can be represented as a vector of size *V*, with its corresponding dimension to be the value of **1** and the rest being **0**'s.
+# - A text can also be represented as a vector of size *V*, with each dimension to be the occurrence (or frequencies) of the words on each dimension (i.e., bag-of-words text vectorization).
 
 # ![](../images/seq-1hot.png)
 
-# - The main problem with is one-hot encoding is that the semantic distances in-between words are all the same, i.e., $D(mice,rats)=D(mice, horses)= 0$.
+# - The main problem with this one-hot encoding of words is that the semantic distances in-between words are all the same, i.e., $D(mice,rats)=D(mice, horses)= 0$.
 
 # ## Word Embeddings
 
-# - Now via neural network, we can learn **word embeddings** automatically. (See Word Embeddings notes).
+# - Now via neural network, we can learn **word embeddings** automatically. (See Word Embeddings lecture notes).
 # - These word embeddings allows us to perform computation of lexical semantics.
 
 # ![](../images/seq-embeddings.png)
@@ -56,13 +57,16 @@ cosine_similarity([mice, rats, horses])
 
 
 # - While each dimension of the word embeddings may not have very transparent **semantic fields**, the results out of the semantic computation do reflect a lot the lexical semantic relations between words.
+# - Please see Mikolov et al's seminal works:
+#     - [Efficient Estimation of Word Representations in Vector Space](https://arxiv.org/pdf/1301.3781.pdf)
+#     - [Distributed Representations of Words and Phrases and their Compositionality](https://arxiv.org/pdf/1310.4546.pdf)
 # 
 
 # ## From One-hot to Embeddings
 
 # - Therefore, we often add **Embedding Layer** as the first layer of a sequence model to render all lexical items in a text into semantically informative embeddings (vectors).
-# - Weights of the Embedding Layer can be trained along with the sequence model.
-# - Or alternatively, we can use pre-trained word embeddings, which were trained in a different task based on a much larger corpus.
+# - Weights of the Embedding Layer can be trained along with the sequence model of the downstream NLP task.
+# - Or alternatively, we can use **pre-trained** word embeddings, which were trained in a different task based on a much larger corpus.
 
 # ![](../images/seq-1hot-to-embeddings.png)
 
@@ -73,34 +77,39 @@ cosine_similarity([mice, rats, horses])
 
 # ## Recurrent Neural Network (RNN) Language Model
 
-# - Words, as semantically represented by embeddings, can now be the input tensors for a RNN.
+# - Words, as vectorized into embeddings, can now be the input tensors for a RNN.
 # 
 # ![](../images/s2s-rnn.jpeg)
 
 # - Moreover, we can create a RNN-based language model.
 # - A language model has two main objectives in its NLP applications:
 #     - To estimate the probability of a given sentence (or any other meaningful linguistic units)
-#     - To predict the upcoming word given limited linguistic inputs
+#     - To predict the upcoming word given the previous limited linguistic inputs
 
 # ![](../images/s2s-rnn-lm.jpeg)
 
-# - The RNN Langage model takes one word at each time step as the input and output the predicted next word.
-# - And the output at time step *i* becomes the input of the RNN at time step *i+1*.
+# - The RNN Langage model takes in one word at a time at each timestep and returns a tensor as the output (hidden state).
+# - And the output at timestep *i* becomes the input of the RNN at timestep *i+1*.
 
-# - The loss function of the RNN Language Model is the distance between the predicted word, *y*, and the correct next-word (in its one-hot form).
-# - The training of the RNN language model is thus to minimize the sum of the **cross-entropy** at all time steps.
+# ### How do we compute the loss of the RNN LM?
+# 
+# - To evaluate the language model, we know the correct next-word, and we have the output tensor from the RNN, we just need a method to compute the difference.
+#     - First, we convert the correct answer (next word) into **one-hot encoding**.
+#     - Second, we make sure that our RNN LM returns a vector of the same size as the one-hot word vector.
+#     - Finally, we compute the loss using **cross-entropy**.
 
 # ![](../images/s2s-rnn-lm-loss.jpeg)
 
-# - For example, if the target next word is *dog*, it's one-hot representation is `[0, 1, 0, 0 ,0, 0]`, and the RNN LM predicted *y* is `[0.2, 0.4, 0.1, 0.1, 0.2, 0.0]`, we can compute the **cross-entropy** at this time step as follows.
+# - For example, if the target next word is *dog*, whose one-hot representation is `[0, 1, 0, 0 ,0, 0]`, and the RNN LM predicted $\hat{y}$ is `[0.2, 0.4, 0.1, 0.1, 0.2, 0.0]`, we can compute the **cross-entropy** at this time step as follows.
 # 
 # $$
 # E = - \sum_{k}t_k \log{y_k}
 # $$
 # 
-#     - k: refers to the dimensions of the one-hot vectors
-#     - t: refers to the target next word
-#     - y: refers to the predicted y from the RNN LM
+# 
+# - $k$: refers to the dimensions of the one-hot vectors
+# - $t$: refers to the target next-word vector
+# - $y$: refers to the predicted $\hat{y}$ from the RNN LM
 # 
 
 # In[1]:
@@ -117,13 +126,13 @@ y = [0.2, 0.4, 0.1, 0.1, 0.2, 0.0]
 cross_entropy(np.array(y), np.array(t))
 
 
-# - And we compute the average cross-entropy error across all time steps for a particular sample (i.e., for the entire sequence).
+# - And we compute the average cross-entropy loss values across all time steps for a particular sample (i.e., for the entire input sequence).
 # 
 # $$
 # E = - \frac{1}{N}\sum_n\sum_{k}t_{nk} \log{y_{nk}}
 # $$
 # 
-#     - N: the number of words in the input text
+# - $N$: the number of words in the input text
 # - We can also compute the average cross-entropy error for samples of a batch size.
 # - We can also compute the average cross-entropy error for the entire training set.
 
@@ -151,9 +160,12 @@ cross_entropy(np.array(y), np.array(t))
 # ### Issues with Vanilla RNN
 # 
 # - An RNN on a sequence of words can be taken as a very deep neural network, where the depths of the network are the number of time steps of the sequence.
-# - In back propagation, for longer sequences, we would run into the **vanishing gradient problems**. To simply put, the gradients would become smaller and smaller as we back propagate further back to the previous contexts of the sequence.
-# - The further back the weights are, the more likely their gradients would approach zero. 
 # 
+# ![](../images/name-gender-classifier-dl/name-gender-classifier-dl.009.jpeg)
+
+# - In back propagation, for longer sequences, we would run into the **vanishing gradient problems**. 
+# - Simply put, the gradients would become smaller and smaller as we back propagate further back to the previous time steps of the sequence.
+# - The further back the weights are, the more likely their gradients would approach zero. 
 
 # ### Why is vanishing gradient an issue?
 # 
@@ -167,15 +179,6 @@ cross_entropy(np.array(y), np.array(t))
 # ### LSTM (Long Short-Term Memory)
 # 
 # - A type of RNN proposed by [Hochreiter and Schmidhuber in 1997](https://www.bioinf.jku.at/publications/older/2604.pdf) as a solution to the vanishing gradients problem.
-# - For every time step, LSTM keeps track of two states: a **hidden** state and a **cell** state
-#     - Both are of the vector length $n$ same as the node/neuron number of the LSTM.
-#     - The **cell** state stores long-term information in the sequence.
-#     - The LSTM can erase, write and read information from the **cell**.
-# - The selection of which information is erased/written/read is controlled by three corresponding **gates**.
-#     - There are three **gates** in LSTM: **output**, **input**, and **forget** gates.
-#     - The gates are also of the vector length $n$ same as the node/neuron number of the LSTM.
-#     - On each time step, each element of the gates can be open(1),closed(0),or somewhere in-between.
-#     - The gates are dynamic: their value is computed based on the current **cell** state.
 
 # - Vanilla RNN
 # 
@@ -186,6 +189,19 @@ cross_entropy(np.array(y), np.array(t))
 # 
 # ![](../images/chris-olah/LSTM3-chain-annotated.jpeg)
 # (Source: http://colah.github.io/posts/2015-08-Understanding-LSTMs/)
+
+# 
+# - For every time step, LSTM keeps track of two states: a **hidden** state and a **cell** state
+#     - Both are of the vector length $n$ same as the node/neuron number of the LSTM.
+#     - The **cell** state stores long-term information in the sequence.
+#     - The LSTM can erase, write and read information from the **cell**.
+#     
+
+# - The selection of which information is erased/written/read is controlled by three corresponding **gates**.
+#     - There are three **gates** in LSTM: **output**, **input**, and **forget** gates.
+#     - The gates are also of the vector length $n$ same as the node/neuron number of the LSTM.
+#     - On each time step, each element of the gates can be open(1),closed(0),or somewhere in-between.
+#     - The gates are dynamic: their value is computed based on the current cell state, hidden state, and the input x.
 
 # - LSTM gates:
 #     - **Input** gate controls how much of the input ($X_t$) is used in computing the new **cell** state ($C_t$)
@@ -208,32 +224,43 @@ cross_entropy(np.array(y), np.array(t))
 
 # ### Heuristics
 # 
+# - LSTM is more complex than GRU.
 # - LSTM is a good default choice (especially if our data has particularly long dependencies, or we have lots of training data)
-# - We may switch to GRUs for **speed** and fewer parameters.
+# - We may switch to GRU for **speed** and fewer parameters.
 
 # ## Variations of Sequence Models
 
-# - There are four major variants of sequence models depending on the types of input and output of the RNN
-#     - Many to Many (Same lengths for input and output sequences)
-#         - Most of the tagging problems fall into this category.
-#     - Many to One
-#         - Most of the classification problems fall into this category.
-#     - One to Many
-#         - Image Captioning
-#     - Many to Many (Variable lengths for input and output sequences)
-#         - Machine Translation
-#         - Chatbot Q&A
-#         - Text Summarization
+# ### Sequence Models 
+# 
+# - Most of the NLP tasks are sequence-to-sequence problems.
+# - Depending on the nature of the tasks as well as their inputs and outputs, we can classify sequence models into four types:
+#     - Many-to-Many
+#     - Many-to-One
+#     - One-to-Many
+#     - Many-to-Many
 
+# ### Many to Many (Same lengths for input and output sequences)
+# 
+# - Most of the tagging problems fall into this category (e.g., POS Tagging, Word Segmentation, Named Entity Recognition)
 # -----
 # ![](../images/seq2seq-m2m-s.jpeg)
 
+# ### Many to One
+# 
+# - Most of the classification problems fall into this category.
 # -----
 # ![](../images/seq2seq-m21.jpeg)
 
+# ### One to Many
+# - Text Generation
+# - Image Captioning
 # -----
 # ![](../images/seq2seq-12m.jpeg)
 
+# ### Many to Many (Variable lengths for input and output sequences)
+# - Machine Translation
+# - Chatbot Q&A
+# - Text Summarization
 # -----
 # ![](../images/seq2seq-m2m-d.jpeg)
 
